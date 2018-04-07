bd81114427ca05243f26af16982d67b00efb60b1
Zhouhongchen <116359398@qq.com>
2018-04-07 19:52:01 +0800

    new style

---
+++ VscAlertView/VscItem.h
@@ -0,0 +1,15 @@
+//
+//  VscItem.h
+//  VscAlertViewDemo
+//
+//  Created by Visac on 2018/4/7.
+//  Copyright © 2018年 SouFun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface VscItem : NSObject
+@property (nonatomic,copy) NSString *title;
+@property (nonatomic,assign) CGFloat height;
+@end
---
+++ VscAlertView/VscItem.m
@@ -0,0 +1,12 @@
+//
+//  VscItem.m
+//  VscAlertViewDemo
+//
+//  Created by Visac on 2018/4/7.
+//  Copyright © 2018年 SouFun. All rights reserved.
+//
+
+#import "VscItem.h"
+
+@implementation VscItem
+@end
